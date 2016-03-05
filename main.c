da5f3b5f529476a41a2313c20f77f110923e8304
David <Iadgarov@users.noreply.github.com>
2016-03-05 10:33:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -33,8 +34,11 @@ typedef struct Instruction {
 	int opcode;
 	int dst, src0, src1;
 	int immediate;
+	char* name;
+	char* toString;
 } Instruction;
 
+void setInstName(Instruction *inst);
 
 Instruction parseInst() {
 	int inst = mem[PC];
@@ -54,6 +58,8 @@ Instruction parseInst() {
 
 	inst >>= 3;
 	iinst.opcode = inst & 0x1f;
+
+	setInstName(&iinst);
 
 	return iinst;
 }
@@ -94,8 +100,7 @@ void execute(Instruction inst) {
 		break;
 
 	case LHI:
-		int x = reg[inst.dst] & 0xffff;
-		reg[inst.dst] = (inst.immediate << 16) | x;
+		reg[inst.dst] = (inst.immediate << 16) | (reg[inst.dst] & 0xffff);
 		break;
 
 	case LD:
@@ -143,27 +148,101 @@ void execute(Instruction inst) {
 }
 
 void writeTrace1(FILE *output, Instruction *inst) {
+
+
 	fprintf(output, "--- instruction %d (%04x) @ PC %d (%04x) -----------------------------------------------------------\n \
-	pc = %04x, inst = %08x, opcode = %d (%s), dst = %d, src0 = %d, src1 = %d, immediate = %16x\n \
+	pc = %04x, inst = %08x, opcode = %d (%s), dst = %d, src0 = %d, src1 = %d, immediate = %08x\n \
 	r[0] = %08x r[1] = %08x r[2] = %08x r[3] = %08x\n \
 	r[4] = %08x r[5] = %08x r[6] = %08x r[7] = %08x\n\n",
 		counter, counter,
 		PC, PC, PC,
 		mem[PC],
-		inst->opcode, inst->dst, inst->src0, inst->src1, inst->immediate,
+		inst->opcode, inst->name, inst->dst, inst->src0, inst->src1, inst->immediate,
 		reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
+
+
+}
+
+void setInstName(Instruction *inst){
+
+	switch (inst->opcode) {
+
+		case ADD:
+			inst->name = "ADD";
+			break;
+
+		case SUB:
+			inst->name = "SUB";
+			break;
+
+		case LSF:
+			inst->name = "LSF";
+			break;
+
+		case RSF:
+			inst->name = "RSF";
+			break;
+
+		case AND:
+			inst->name = "AND";
+			break;
+
+		case OR:
+			inst->name = "OR";
+			break;
+
+		case XOR:
+			inst->name = "XOR";
+			break;
+
+		case LHI:
+			inst->name = "LHI";
+			break;
+
+		case LD:
+			inst->name = "LD";
+			break;
+
+		case ST:
+			inst->name = "ST";
+			break;
+
+		case JLT:
+			inst->name = "JLT";
+			break;
+
+		case JLE:
+			inst->name = "JLE";
+			break;
+
+		case JEQ:
+			inst->name = "JEQ";
+			break;
+
+		case JNE:
+			inst->name = "JNE";
+			break;
+
+		case JIN:
+			inst->name = "JIN";
+			break;
+
+		}
 }
 
 void run() {
 	Instruction inst;
 
-	output = fopen("trace.txt", "w");
+
 
 	while (1) {
 		inst = parseInst();
 		if (inst.opcode == HLT) {
 			break;
 		}
+
+
+		writeTrace1(output, &inst);	// before
 
 		if (PC == 0xffff) {
 			PC = 0;
@@ -172,30 +251,49 @@ void run() {
 			PC++;
 		}
 
-		writeTrace1(output, &inst);
 
-		execute(inst);
 
-		fprintf(output, ">>>> EXEC: R[%d] = %d ADD 0 <<<<\n\n");
+		execute(inst); // after
+
+		fprintf(output, ">>>> EXEC: R[%d] = %d %s 0 <<<<\n\n", inst.dst, reg[inst.dst], inst.name);
 
 		counter++;
 
 	}
 
-	fclose(output);
+
 
 }
 
 void readMem() {
 	input = fopen(filename, "r");
+	char line[9] = {0};
 
 	int addr = 0;
-	while (fgets(line, 9, input) != NULL) {
+	while (fgets(line, 10, input) != NULL) {
 		mem[addr] = strtol(line, NULL, 16);
 		addr++;
+
 	}
 
+	fprintf(output, "program %s opened, %d lines\n\n", filename, addr);
 	fclose(input);
+}
+
+int main(int argc, char *argv[]){
+
+
+	output = fopen("trace.txt", "w+");
+
+	reg[0] = 0;
+	reg[1] = 0xf;
+	filename = argv[1];
+
+	readMem();
+	run();
+
+	fclose(output);
+	return 0;
 }
 
 
